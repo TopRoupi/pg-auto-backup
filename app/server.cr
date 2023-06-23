2215ebfbefb468278144a66d9ba8992c5bda8a46
a <breno.nunesgalvao@protonmail.ch>
2023-06-23 18:28:10 -0300

    add login

--- app/server.cr
+++ app/server.cr
@@ -1,5 +1,6 @@
 # require shards
 require "kemal"
+require "kemal-session"
 require "shrine"
 require "uri"
 require "dotenv"
@@ -8,11 +9,23 @@ if File.exists?("./.env")
   Dotenv.load
 end
 
+Kemal::Session.config do |config|
+  config.cookie_name = "session_id"
+  config.secret = ENV["SECRET_KEY"]
+  config.gc_interval = 2.minutes # 2 minutes
+end
+
 require "./shrine.cr"
 require "./helpers.cr"
 
 require "./models/backup.cr"
 require "./models/restore.cr"
+
+before_get "/" do |env|
+  if env.request.path != "/login"
+    env.redirect "/login" if env.session.int?("admin") == nil
+  end
+end
 
 get "/" do
   objs = AWS_CLIENT.list_objects(ENV["AWS_BUCKET"]).first.contents.sort_by do |s|
@@ -20,6 +33,20 @@ get "/" do
   end.reverse
 
   render_page "home"
+end
+
+get "/login" do
+  render_page "login"
+end
+
+post "/login" do |env|
+  key = env.params.body["password"].as(String)
+  if key != ENV["ACCESS_PASSWORD"]
+    env.redirect "/login"
+  else
+    env.session.int("admin", 1)
+    env.redirect "/"
+  end
 end
 
 post "/backup" do
